542d0e92dae49c4640f032dcdfc37a82a05f0226
Rafael Fernández López <ereslibre@ereslibre.es>
2025-07-07 17:47:30 +0200

    feat: implement simulations

--- src/business/simulations.rs
+++ src/business/simulations.rs
@@ -27,3 +27,104 @@
 //! [^note]: This feature is **only** available in the sandbox
 //! environment. Trying to use such a feature using a production
 //! client will result in an error at compile time.
+
+use crate::{
+    business::client::{self, BusinessAuthentication, Environment, HttpMethod},
+    client::{Body, Client, SandboxEnvironment},
+    errors::ApiResult,
+};
+
+pub mod v10 {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Clone, Debug, strum::Display)]
+    #[strum(serialize_all = "snake_case")]
+    pub enum TransferStateRequest {
+        Complete,
+        Revert,
+        Decline,
+        Fail,
+    }
+
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct TransferStateUpdate {
+        pub id: String,
+        pub state: TransferState,
+        pub created_at: String,
+        pub completed_at: Option<String>,
+    }
+
+    #[derive(Clone, Debug, Deserialize, strum::Display, Serialize)]
+    #[serde(rename_all = "snake_case")]
+    #[strum(serialize_all = "snake_case")]
+    pub enum TransferState {
+        #[serde(alias = "COMPLETED")]
+        Completed,
+        #[serde(alias = "REVERTED")]
+        Reverted,
+        #[serde(alias = "DECLINED")]
+        Declined,
+        #[serde(alias = "FAILED")]
+        Failed,
+    }
+
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct TopUpRequest {
+        pub account_id: String,
+        pub amount: f64,
+        pub currency: String,
+        pub reference: Option<String>,
+        pub state: Option<TopUpState>,
+    }
+
+    #[derive(Clone, Debug, Deserialize, Serialize)]
+    pub struct TopUp {
+        pub id: String,
+        pub state: TopUpState,
+        pub created_at: String,
+        pub completed_at: Option<String>,
+    }
+
+    #[derive(Clone, Debug, Deserialize, strum::Display, Serialize)]
+    #[serde(rename_all = "snake_case")]
+    #[strum(serialize_all = "snake_case")]
+    pub enum TopUpState {
+        #[serde(alias = "PENDING")]
+        Pending,
+        #[serde(alias = "COMPLETED")]
+        Completed,
+        #[serde(alias = "REVERTED")]
+        Reverted,
+        #[serde(alias = "FAILED")]
+        Failed,
+    }
+}
+
+pub async fn transfer_state_update(
+    client: &Client<SandboxEnvironment<client::BusinessClient>, BusinessAuthentication>,
+    id: &str,
+    state: &v10::TransferStateRequest,
+) -> ApiResult<v10::TransferStateUpdate> {
+    client
+        .request(
+            HttpMethod::Post::<()> { body: None },
+            &client
+                .environment
+                .uri("1.0", &format!("/sandbox/transactions/{id}/{state}")),
+        )
+        .await
+}
+
+pub async fn account_top_up(
+    client: &Client<SandboxEnvironment<client::BusinessClient>, BusinessAuthentication>,
+    top_up: &v10::TopUpRequest,
+) -> ApiResult<v10::TopUp> {
+    client
+        .request(
+            HttpMethod::Post {
+                body: Some(Body::Json(top_up)),
+            },
+            &client.environment.uri("1.0", "/sandbox/topup"),
+        )
+        .await
+}
