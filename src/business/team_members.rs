b7f85e028632b6935470639589ce302e7980e7ed
Rafael Fernández López <ereslibre@ereslibre.es>
2025-07-09 23:49:13 +0200

    feat: implement transactions

--- src/business/team_members.rs
+++ src/business/team_members.rs
@@ -106,10 +106,7 @@ impl std::fmt::Display for v10::ListParams {
         .iter()
         .fold(String::new(), |acc, (key, value)| {
             if let Some(value) = value {
-                let value = percent_encoding::utf8_percent_encode(
-                    value,
-                    percent_encoding::NON_ALPHANUMERIC,
-                );
+                let value = urlencoding::encode(value);
                 if acc.is_empty() {
                     format!("{acc}?{key}={}", value)
                 } else {
