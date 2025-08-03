575653fca064bdd60843fe758a242fc5fb718259
Rafael Fernández López <ereslibre@ereslibre.es>
2025-08-03 17:40:48 +0200

    chore: update

--- devenv.nix
+++ devenv.nix
@@ -27,7 +27,7 @@
   scripts = {
     setup-business-api.exec = ''
       if [ -d .setup-business-api ]; then
-        echo "Remove the .setup-business-api directory before continuing if you want to start from scratch"
+        echo "Remove the .setup-business-api directory before continuing if you want to start from scratch."
         echo
         pushd .setup-business-api &> /dev/null
       else
